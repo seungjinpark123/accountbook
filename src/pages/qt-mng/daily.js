7795e931f146a508ee7ec95eb4b3a684f7f756d9
seungjin park <sparkpsalm@gmail.com>
2023-04-29 22:08:39 +0900

    테스트커밋

--- src/pages/qt-mng/daily.js
+++ src/pages/qt-mng/daily.js
@@ -1,11 +1,7 @@
-import React from "react";
+import React from 'react';
 
-const Daily = ()=>{
-    return(
-        <div>
-           하이루. 데일리 페이지입니다.
-        </div>
-    )
-}
+const Daily = () => {
+  return <div>하이루. 데일리 페이지입니다. 원격 테스트.</div>;
+};
 
 export default Daily;
